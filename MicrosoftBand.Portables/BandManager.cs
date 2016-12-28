7031092df564dacfaf48797e59408a8d60f367ba
acsattila <acsattila904@gmail.com>
2016-12-28 22:54:47 +0100

    Add project files.

---
+++ MicrosoftBand.Portables/BandManager.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Microsoft.Band;
+
+namespace MicrosoftBand.Portables
+{
+    /// <summary>
+    /// Manage a connection to selected Band device
+    /// The lifecycle of the Manager handle the Band Connection lifecycle (Dispose)
+    /// </summary>
+    public class BandManager : IDisposable
+    {
+        /// <summary>
+        /// paired band infos (paired to operation system not to app)
+        /// </summary>
+        public IBandInfo[] PairedBands { get; private set; }
+
+        /// <summary>
+        /// number of band, that can we use
+        /// </summary>
+        public int NumberOfPairedBands => PairedBands?.Length ?? 0;
+
+        /// <summary>
+        /// Actual selected, and connected Band client
+        /// </summary>
+        public IBandClient BandClient { get; private set; }
+
+        /// <summary>
+        /// Connection is initialized and connected to 
+        /// </summary>
+        public bool Initialized { get; private set; }
+
+        private BandManager()
+        {
+            
+        }
+
+        /// <summary>
+        /// Create a BandManager object to get paired Microsoft Band devices
+        /// </summary>
+        public static async Task<BandManager> CreateAsync()
+        {
+            //Get the paired Bands
+            var manager = new BandManager
+            {
+                PairedBands = await BandClientManager.Instance.GetBandsAsync(),
+                Initialized = true
+            };
+
+            return manager;
+        }
+
+        /// <summary>
+        /// Create a connection (Client) to selected Band device
+        /// </summary>
+        /// <param name="numberOfConnectedBand">which ndex of Band device select to connection</param>
+        /// <returns></returns>
+        public async Task ConnectToAsync(int numberOfConnectedBand)
+        {
+            if (PairedBands == null || PairedBands.Length == 0)
+                throw new ActionNotSupportedException("Can't find Microsoft Band Devide");
+
+            try
+            {
+                BandClient = await BandClientManager.Instance.ConnectAsync(PairedBands[numberOfConnectedBand]);
+                //Connected Successfully
+                await BandClient.NotificationManager.VibrateAsync(Microsoft.Band.Notifications.VibrationType.RampUp);
+            }
+            catch (BandException ex)
+            {
+                // handle a Band connection exception
+                Debug.WriteLine("Can't connect to Band");
+            }
+        }
+
+        /// <summary>
+        /// Create a connection with the selected index of Band device
+        /// </summary>
+        /// <param name="numberOfConnectedBand"></param>
+        /// <returns></returns>
+        public static async Task<BandManager> CreateConnectionAsync(int numberOfConnectedBand = 0)
+        {
+            var manager = await BandManager.CreateAsync();
+
+            await manager.ConnectToAsync(numberOfConnectedBand);
+
+            return manager;
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                BandClient.Dispose();
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine("Can't dispose to Band");
+            }
+            finally
+            {
+                Initialized = false;
+            }
+        }
+    }
+}
---
+++ MicrosoftBand.ThemeSelector/MainPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Band;
+using MicrosoftBand.Portables;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MicrosoftBand.ThemeSelector
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private async void MainPage_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var bandManager = await BandManager.CreateConnectionAsync())
+                {
+                    tbFirmwareVersion.Text = await bandManager.BandClient.GetFirmwareVersionAsync();
+                    tbHardwareVersion.Text = await bandManager.BandClient.GetHardwareVersionAsync();
+                }
+            }
+            catch (Exception)
+            {
+                MessageDialog dialog = new MessageDialog("Nem elérhető a Band");
+                await dialog.ShowAsync();
+            }
+        }
+
+        private async void BtnSubmitColor_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (var bandManager = await BandManager.CreateConnectionAsync())
+                {
+                    BandTheme theme = await bandManager.BandClient.PersonalizationManager.GetThemeAsync();
+
+                    theme.Base = GetSliderColor().ToBandColor();
+
+                    await bandManager.BandClient.PersonalizationManager.SetThemeAsync(theme);
+                }
+            }
+            catch (Exception)
+            {
+                MessageDialog dialog = new MessageDialog("Nem elérhető a Band");
+                await dialog.ShowAsync();
+            }
+        }
+
+        private void Slider_OnValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            rectBackground.Fill = new SolidColorBrush(GetSliderColor());
+        }
+
+        private Color GetSliderColor()
+        {
+            byte r = Convert.ToByte(sliderRed.Value);
+            byte g = Convert.ToByte(sliderGreen.Value);
+            byte b = Convert.ToByte(sliderBlue.Value);
+
+            return Color.FromArgb(255, r, g, b);
+        }
+    }
+}
